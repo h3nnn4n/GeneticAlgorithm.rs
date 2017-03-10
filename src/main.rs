fa6299cca8984dbe43bed93530e6d76f950f62db
h3nnn4n <uber.renan@gmail.com>
2017-03-10 17:23:17 +0000

    WIP. Added mutation (for f64) and soem helper functions

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,7 @@ struct Individual<T> {
     ub: T,
     lb: T,
     mutation_chance: f64,
+    fitness: f64,
 }
 
 #[allow(dead_code)]
@@ -39,6 +40,13 @@ impl<T> Population<T> {
             ub: ub,
             mutation_chance: 0.05,
             crossover_chance: 0.8,
+        }
+    }
+
+    fn set_mutation_chance(&mut self, chance: f64){
+        self.mutation_chance = chance;
+        for ref mut x in &mut self.individuals {
+            x.set_mutation_chance(chance);
         }
     }
 }
@@ -78,11 +86,42 @@ impl Population<f64> {
         }
     }
 
-    fn set_mutation_chance(&mut self, chance: f64){
-        self.mutation_chance = chance;
+    fn mutate_everyone(&mut self){
         for ref mut x in &mut self.individuals {
-            x.set_mutation_chance(chance);
+            x.mutate();
         }
+    }
+
+    fn print_pop_fit(&self){
+        for ref x in &self.individuals {
+            x.print_fitness();
+        }
+        println!();
+    }
+
+    fn print_best_fit(&self){
+        let mut best = self.individuals[0].fitness;
+        let mut best_obj = self.individuals[0].objective_function();
+        for ref x in &self.individuals {
+            let w = x.fitness;
+            if w > best {
+                best_obj = x.objective_function();
+                best = w;
+            }
+        }
+        println!("{} {}", best, best_obj );
+        //println!();
+    }
+
+    fn get_best(&self) -> Individual<f64> {
+        let mut pos = 0;
+        for i in 1..self.individuals.len(){
+            if self.individuals[pos].fitness < self.individuals[i].fitness {
+                pos = i;
+            }
+        }
+
+        return self.individuals[pos].clone();
     }
 }
 
@@ -115,20 +154,47 @@ impl Individual<f64> {
             let w: f64 = between.ind_sample(&mut rng);
             self.gene.push(w);
         }
+
+        self.fitness = self.fitness();
+    }
+
+    fn objective_function(&self) -> f64 {
+        return rastrigin(self.gene.as_slice());
+    }
+
+    fn fitness(&self) -> f64 {
+        let f = self.objective_function();
+        if f < 0.0001 {
+            return 100000.;
+        } else {
+            return 1.0/f;
+        }
     }
 
     fn print(&self) {
         for i in 0..self.gene.len() {
             println!("{0:4} {1}", i, self.gene[i]);
         }
+        self.print_fitness();
+    }
+
+    fn print_fitness(&self) {
+        println!("Fitness: {}", self.fitness());
     }
 
     fn mutate(&mut self){
-        unimplemented!();
-    }
+        //let p = rand::thread_rng().gen_range(0, self.gene.len());
+        let mut changed:bool = false;
+        for p in 0..self.gene.len() {
+            if rand::thread_rng().gen::<f64>() < self.mutation_chance {
+                self.gene[p] = self.gene[p] + rand::thread_rng().gen::<f64>() - 0.5;
+                changed = true;
+            }
+        }
 
-    fn fitness(self) -> f64 {
-        return -rastrigin(self.gene.as_slice());
+        if changed {
+            self.fitness = self.fitness();
+        }
     }
 }
 
@@ -159,6 +225,7 @@ impl<T> Individual<T> {
             ub: ub,
             lb: lb,
             mutation_chance: 0.05,
+            fitness: -1.,
         }
     }
 
@@ -168,14 +235,22 @@ impl<T> Individual<T> {
 }
 
 fn main () {
-    let n_dim = 10;
+    let n_dim = 2;
     let pop_size = 10;
+    let max_iter = 1000;
+
     let mut pop:Population<f64> = Population::<f64>::new(pop_size, n_dim, -5.12, 5.12);
+
     pop.init();
     pop.set_mutation_chance(0.1);
 
-    for x in pop.individuals {
-        x.print();
-        println!();
+    //pop.print_pop_fit();
+
+    for _ in 0..max_iter{
+        let best = pop.get_best();
+        pop.print_best_fit();
+        pop.mutate_everyone();
+
+        pop.individuals[0] = best;
     }
 }
